5731e74dd9631e66ff43d43f178397125b0a93b1
Avi Wilensky <avi@promediacorp.com>
2017-01-08 14:07:02 -0500

    front end

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 // Initialize Firebase
 // TODO: Replace with your project's customized code snippet
+var Firebase = require("firebase");
+
 var config = {
-  apiKey: "<API_KEY>",
-  authDomain: "<PROJECT_ID>.firebaseapp.com",
-  databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
-  storageBucket: "<BUCKET>.appspot.com",
+  apiKey: "AIzaSyBeaFhvd9_lGuyM5Esms3YnFlayi0exfDw",
+  authDomain: "partycrashers-b31af.firebaseapp.com",
+  databaseURL: "https://partycrashers-b31af.firebaseio.com",
+  storageBucket: "gs://partycrashers-b31af.appspot.com",
 };
+
 firebase.initializeApp(config);
+
+
+var ref = firebase.database().ref();
+
+ref.on("value", function(snapshot) {
+  console.log(snapshot.val());
+  }, function (error) {
+  console.log("Error: " + error.code);
+});
